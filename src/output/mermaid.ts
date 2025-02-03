dd0eed92a06e21ea5cf9fd4988c36d1f61e4bf33
Klemeesi <klemeesi@gmail.com>
2025-02-03 21:16:43 +0200

    28 First version of Mermaid support

---
+++ src/output/mermaid.ts
@@ -0,0 +1,43 @@
+import type { ImportInfoV2, MermaidFormatOptions } from "../types";
+import * as fs from "fs";
+import path from "path";
+
+const defaultOptions = {
+    dir: "TB",
+    nodeId: "id",
+    outputPath: "exports",
+    outputName: "export",
+};
+
+const encodeText = (text: string) => text.replace("@", "#64;");
+const encodeId = (id: string) => id.replace("@", "_");
+
+const getNodeGenerator = (opt: MermaidFormatOptions) => (i: ImportInfoV2) => ({
+    id: encodeId((i[opt.nodeId!] as string) || i.uniqueId),
+    name: encodeText(i.moduleName),
+    link: i.type === "Source file" ? "==>" : i.type === "Node module" ? "-->" : "-.-",
+});
+
+const returnMermaidLinks = (root: ImportInfoV2, getInfo: ReturnType<typeof getNodeGenerator>) => {
+    const src = getInfo(root);
+    let lines: string[] = [];
+    root.imports.forEach((dest) => {
+        const dst = getInfo(dest);
+        lines = [...lines, `${src.id}[${src.name}] ${dst.link} ${dst.id}[${dst.name}]`, ...returnMermaidLinks(dest, getInfo)];
+    });
+    return lines;
+};
+
+export const mermaidOutputPlugin = (options: MermaidFormatOptions) => async (imports: ImportInfoV2[]) => {
+    const opt = { ...defaultOptions, ...(options || {}) };
+    const output = path.resolve(opt.outputPath, `${opt.outputName}.md`);
+
+    let lines: string[] = [];
+    imports.forEach((root) => {
+        lines = [...lines, ...returnMermaidLinks(root, getNodeGenerator(options))];
+    });
+    const content = ["```mermaid", `graph ${opt.dir}`, ...lines, "```"].join("\n");
+
+    fs.writeFileSync(output, content);
+    console.log("Saved", output);
+};
